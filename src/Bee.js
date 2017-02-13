4f76c41265096c44685023319c17d605638bb57a
Misaki Matsumoto <misaki.matsumoto2524@gmail.com>
2017-02-13 14:24:04 -0800

    Done

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,15 @@
 var Bee = function() {
+  Grub.call(this, this.food, this.eat);
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
+
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
+
+
+
+
+
 
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,23 @@
 var HoneyMakerBee = function() {
+  Bee.call(this, this.color, this.food, this.eat);
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
 };
+
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot += 1;
+  return this.honeyPot;
+};
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot -= 1;
+  return this.honeyPot;
+};
+
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+
 
